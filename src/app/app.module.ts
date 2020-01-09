588b4bb09511381b2c26b0d5f5c236e53b0ddfc9
Bobby Roe <rroe@seattlecca.org>
2020-01-09 11:08:07 -0800

    add @nebular/themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./product-list/product-list.component";
-import { PoductAlertsComponent } from './poduct-alerts/poduct-alerts.component';
+import { PoductAlertsComponent } from "./poduct-alerts/poduct-alerts.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent, PoductAlertsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: "cosmic" }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/poduct-alerts/poduct-alerts.component.ts
+++ src/app/poduct-alerts/poduct-alerts.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-poduct-alerts',
-  templateUrl: './poduct-alerts.component.html',
-  styleUrls: ['./poduct-alerts.component.css']
+  selector: "app-poduct-alerts",
+  templateUrl: "./poduct-alerts.component.html",
+  styleUrls: ["./poduct-alerts.component.css"]
 })
 export class PoductAlertsComponent implements OnInit {
+  @Input() product;
+  @Output() notify = new EventEmitter();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
